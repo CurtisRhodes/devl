65af4d1e07b4ff6e87c63a8f8d42631e26231834
Curtis Rhodes <curtishrhodes@hotmail.com>
2022-03-28 21:56:49 -0500

    good nite

--- admin/js/OggleMetrics.js
+++ admin/js/OggleMetrics.js
@@ -6,15 +6,16 @@ function showPageHitsReport() {
     $('#pageHitReports').show();
     
     // daily page hits report
-    let sql = `select Occured, format(count(*),0) count from PageHit group by Occured`;
+    let sql = "select date_format(Occured,'%b %e') as FormattedDate, Occured, format(count(*),0) as count from PageHit group by Occured";
     $.ajax({
         url: "php/registroFetchAll.php?query=" + sql,
         success: function (response) {
             let errorSummary = JSON.parse(response);
-            $('#pageHitSummaryReport').html("<div style='text-align=\"center\"'>PageHit Summary</div>")
+            $('#pageHitSummaryReport').html("<div  class='reportBodyTitle'>PageHit Summary</div>")
             let tableKlude = "<table>";  // <th><tr><td colspan=2 text-algn='center'>PageHit Summary</td><tr></th>";
             $.each(errorSummary, function (idx, obj) {
-                tableKlude += "<td>" + obj.Occured + "</td>";
+                tableKlude += "<td class='clickable underline' onclick='hitsByPageReport(\"" + obj.Occured + "\")'>" + obj.FormattedDate + "</td>";
+
             });
             tableKlude += "</tr><tbody></tr>"
             $.each(errorSummary, function (idx, obj) {
@@ -29,6 +30,38 @@ function showPageHitsReport() {
         }
     });
 }
+function hitsByPageReport(pageHitDate) {
+
+    let sql = `select  count(*) as Hits, PageId, FolderName, RootFolder 
+                from PageHit h join st21569_yagdrassel.CategoryFolder f on f.Id = h.PageId
+                where Occured='`+ pageHitDate +`' group by PageId, FolderName, Rootfolder
+                order by count(*) desc`;
+
+    $.ajax({
+        url: "php/registroFetchAll.php?query=" + sql,
+        success: function (response) {
+
+            let errorSummary = JSON.parse(response);
+            $('#pageHitByPageReport').html("<div class='reportBodyTitle'>PaegeHit Detail " + pageHitDate + "</div>");
+
+            let tableKlude = "<table>";
+            $.each(errorSummary, function (idx, obj) {
+                tableKlude += "<tr><td>" + obj.Hits + "</td>";
+                tableKlude += "<td class='clickable underline' onclick='pageHitPageDetailReport(\"" + obj.PageId + "\")'>" + obj.FolderName + "</td>";
+                tableKlude += "<td>" + obj.RootFolder + "</td></tr>";
+            });
+            tableKlude += "<table>"
+            $('#pageHitByPageReport').append(tableKlude).show();
+        },
+        error: function (jqXHR) {
+            let errMsg = getXHRErrorDetails(jqXHR);
+            alert("pageHit by pag eReport: " + errMsg);
+        }
+    });
+
+}
+
+function pageHitPageDetailReport()
 
 function showDailyErrorReport() {
     closeAllReports();
@@ -47,11 +80,11 @@ function showDailyErrorReport() {
         url: "php/registroFetchAll.php?query=" + sql,
         success: function (response) {
             let errorSummary = JSON.parse(response);
-            //$('#errorSummaryReport').html("<div>Error Summary</div>")
-            let tableKlude = "<table><th><tr><td colspan=2>Error Summary</td></tr></th>";
+            $('#errorSummaryReport').html("<div class='reportBodyTitle'>Error Summary</div>")
+            let tableKlude = "<table>";  // <th><tr><td colspan=2>Error Summary</td></tr></th>";
             $.each(errorSummary, function (idx, obj) {
                 tableKlude += "<tr><td>" + obj.Count + "</td>";
-                tableKlude += "<td onclick='errorDetailReport(" + obj.ErrorCode +")'>" + obj.ErrorCode + "</td>";
+                tableKlude += "<td class='clickable underline' onclick='errorDetailReport(\"" + obj.ErrorCode + "\")'>" + obj.ErrorCode + "</td>";
                 tableKlude += "<td>" + obj.Description + "</td>";
                 tableKlude += "<td>" + obj.CalledFrom + "</td>";
                 tableKlude += "<td>" + obj.Occured + "</td></tr>";
@@ -65,29 +98,31 @@ function showDailyErrorReport() {
         }
     });
 }
-
 function errorDetailReport(errorCode) {
 
     let sql = `select ErrorCode, r.Description, CalledFrom, FolderId, f.FolderName, ErrorMessage, City,Region,Country, e.Occured
             from ErrorLog e left join RefCode r on e.ErrorCode = r.RefCode
             left join st21569_yagdrassel.CategoryFolder f on f.Id = e.FolderId
             left join Visitor v on e.VisitorId = v.VisitorId
-            where ErrorCode='` + errorCode + `' order by e.Occured desc`;
+            where ErrorCode="` + errorCode + `" order by e.Occured desc`;
 
     $.ajax({
         url: "php/registroFetchAll.php?query=" + sql,
         success: function (response) {
 
             let errorSummary = JSON.parse(response);
-            $('#errorDetailReport').html("<div>Error Detail</div>")
+            $('#errorDetailReport').html("<div class='reportBodyTitle'>Error Detail</div>")
+            $('#errorDetailReport').html("<div>Error" + errorSummary[0].ErrorCode + ", " + errorSummary[0].Description + "</div>")
 
             let tableKlude = "<table>";
             $.each(errorSummary, function (idx, obj) {
-                tableKlude += "<tr><td>" + obj.Count + "</td>";
-                tableKlude += "<td>" + obj.ErrorCode + "</td>";
-                tableKlude += "<td>" + obj.Description + "</td>";
+                tableKlude += "<tr><td>" + obj.ErrorMessage + "</td>";
+                tableKlude += "<td>" + obj.Occured + "</td>";
                 tableKlude += "<td>" + obj.CalledFrom + "</td>";
-                tableKlude += "<td>" + obj.Occured + "</td></tr>";
+                tableKlude += "<td>" + obj.FolderId + "</td>";
+                tableKlude += "<td>" + obj.City + "</td>";
+                tableKlude += "<td>" + obj.Region + "</td>";
+                tableKlude += "<td>" + obj.Country + "</td></tr>";
             });
             tableKlude += "<table>"
             $('#errorDetailReport').append(tableKlude).show();
@@ -101,11 +136,10 @@ function errorDetailReport(errorCode) {
 }
 
 function closeAllReports() {
-    $('.fullScreenContainer').hide();  // close all other sections
+    $('.fullScreenSection').hide();  // close all other sections
     $('.reportSection').hide();  // close all other sections
     $('#reportsHeader').hide();
 }
-
 
 function verifyHITConnection() {
 
