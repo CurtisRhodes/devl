9585537563961d101085615c933390fca1967c64
Curtis Rhodes <curtishrhodes@hotmail.com>
2022-05-06 14:25:36 -0500

    dirtree

--- OggleBooble.com/admin/js/DirTreeJson.js
+++ OggleBooble.com/admin/js/DirTreeJson.js
@@ -72,6 +72,51 @@ function recurseDirTree(nodeId) {
     }
 }
 
+function recurseForHtml(nodeId) {
+    try {
+        let parentNode = deepLookup(nodeId);
+        if (isNullorUndefined(jsonResultsItem))
+            console.log(nodeId + " sb a terminal node");
+        else {
+            let subNodes = dirTreeArray.filter(node => node.Parent == nodeId);
+            $.each(subNodes, function (idx, subNode) {
+                if (subNode.IsStepChild == 0) {
+
+
+                    let folderImage = "https://common.ogglebooble.com/img/redballon.png";
+                    if (!isNullorUndefined(subNode.FolderImage)) folderImage = settingsImgRepo + subNode.FolderImage;
+                    let fileCount = showFileCountTxt(subNode);
+
+                    parentNode.nodeContainer.append();
+
+
+                    sbDirTree.Append(
+                        "<div class='dirTreeNode clickable' style='text-indent:" + dirTreeTab + "px'>"
+                        + "<span id='DQ33" + randomId + "' onclick='toggleDirTree(\"" + randomId + "\")' >[" + expandMode + "] </span>"
+                        + "<div id='" + randomId + "aq' class='" + treeNodeClass + "' "
+                        + "onclick='commonDirTreeClick(\"" + vwDir.DanniPath + "\"," + vwDir.ThisNode.Id + ")' "
+                        + "oncontextmenu='showDirTreeContextMenu(" + vwDir.ThisNode.Id + ")' "
+                        + "onmouseover='showFolderImage(\"" + folderImage + "\")' onmouseout='hideFolderImage()'>"
+                        + vwDir.ThisNode.FolderName + "</div><span class='fileCount'>  : "
+                        + txtFileCount + "</span></div>" + "\n<div class='" + expandClass + "' id='Q88" + randomId + "'>");
+
+                    jsonResultsItem.nodes.push({
+                        "Id": "" + subNode.Id + "", "folderName": "" + subNode.FolderName +
+                            "", "folderPath": "" + subNode.FolderPath + "", "folderImage": "" + folderImage + "", "fileCount": "" + fileCount + "", "nodes": []
+                    });
+                }
+            });
+            $.each(subNodes, function (idx, subNode) {
+                if (subNode.IsStepChild == 0) {
+                    recurseForHtml(subNode.Id);
+                }
+            });
+        }
+    } catch (e) {
+        logAdminError("CAT", e, "recurseDirTree");
+    }
+}
+
 function deepLookup(searchNodeId) {
     let resultFound = false;
     let success = null;
