6f0e0810b2d6fc6d3668d4e8e8a5858b5410832d
Curtis Rhodes <curtishrhodes@hotmail.com>
2022-05-22 09:13:10 -0500

    KSKD

--- OggleBooble.com/php/moveToRejects.php
+++ OggleBooble.com/php/moveToRejects.php
@@ -8,13 +8,25 @@
         $imageFileId = $_GET['imageFileId'];
         $rejectFolderId = -6;
 
-            $sql = "update ImageFile set FolderId=".$rejectFolderId.", RootFolder='reject' where Id=".$imageFileId;
-        }
+        $sql = "update ImageFile set FolderId=".$rejectFolderId." where Id='".$imageFileId."'";
         $stmt= $pdo->prepare($sql);
         $stmt->execute();
-
-        $success="ok";
-
+        $stmt1Success = $stmt->errorCode();
+        if($stmt1Success == '00000') {
+            $sql = "update CategoryImageLink SET ImageCategoryId=".$rejectFolderId." WHERE ImageLinkId='".$imageFileId."'";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute();
+            $stmt1Success = $stmt->errorCode();
+            if($stmt1Success == '00000') {
+                $success = 'ok';
+            }
+            else {
+                $success = $stmt1Success." sql: ".$sql;
+            }
+        }
+        else {
+            $success = $stmt1Success." sql: ".$sql;
+        }
         $pdo = null;
     }
     catch(Exception $e) {
