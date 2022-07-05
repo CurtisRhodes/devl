13d56c6791322d8a8908d2c5cc12b632c7c60ff0
Curtis Rhodes <curtishrhodes@hotmail.com>
2022-07-04 20:52:04 -0500

    cors push

--- OggleBooble.com/php/yagdrassel.php
+++ OggleBooble.com/php/yagdrassel.php
@@ -7,12 +7,31 @@ function pdoConn() {
      $user_name = 'oggleboo_yagdrassel';
      $password = 'Terry_ecac8432-304a-44be-ac2b-76559b4b0e85';
 
+
     try {
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+    
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        }
+    
     	return new PDO('mysql:host=' . $host_name . ';dbname=' . $database . ';charset=utf8', $user_name, $password);
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
     	exit('Failed to connect to database!');
     }
-}
 
 ?>
